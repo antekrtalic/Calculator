40df828af3a3e75ebb079146b6b31d08668ebd34
Ante Dragoje <antekrtalic2@gmail.com>
2021-05-05 15:59:35 +0200

    Added functionality to buttons

--- script.js
+++ script.js
@@ -31,17 +31,66 @@ function operate(oper, num1, num2) {
     }
 }
 
-let paragraph = document.getElementsByTagName('p');
+
 
 window.addEventListener('keydown', function(e) {
-    if (e.keyCode === 104) {
-        paragraph[0].innerHTML = "";
+    const paragraph = document.querySelector('p');
+    
+    switch(e.key) {
+        case "Delete":
+            paragraph.innerHTML = "";
+            break;
+        case "Escape":
+            paragraph.innerHTML = "";
+            break;
+        case "1": 
+            paragraph.innerHTML += "1";
+            break;
+        case "2": 
+            paragraph.innerHTML += "2";
+            break;
+        case "3": 
+            paragraph.innerHTML += "3";
+            break;
+        case "4": 
+            paragraph.innerHTML += "4";
+            break;
+        case "5": 
+            paragraph.innerHTML += "5";
+            break;
+        case "6": 
+            paragraph.innerHTML += "6";
+            break;
+        case "7": 
+            paragraph.innerHTML += "7";
+            break;
+        case "8": 
+            paragraph.innerHTML += "8";
+            break;
+        case "9": 
+            paragraph.innerHTML += "9";
+            break;
+        case ",":
+            paragraph.innerHTML += ".";
+            break;
+        case "+":
+            paragraph.innerHTML += "+";
+            break;
+        case "-":
+            paragraph.innerHTML += "-";
+            break;
+        case "*":
+            paragraph.innerHTML += "*";
+            break;
+        case "/":
+            paragraph.innerHTML += "/";
+            break;
+        case "Enter":
+            operate();
+            break;
+        default:
+            break;
     }
-    if (e.key === "Esc") {
-        paragraph[0].textContent = "";
-    } else {
-        paragraph[0].textContent += e.key;
-    }
-    console.log(e);
+    console.log(e.key);
     
 });
