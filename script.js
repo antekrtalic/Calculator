41aa1f359faa38e637c98b39ffdcf2ea4fe2793d
Ante Dragoje <antekrtalic2@gmail.com>
2021-05-05 11:32:42 +0200

    Added event listeners that respond to buttons for calculator

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+
+function operate(oper, num1, num2) {
+    if (oper === "+") {
+        add(num1, num2);
+    } else if (oper === "-") {
+        subtract(num1, num2);
+    } else if (oper === "*") {
+        multiply(num1, num2);
+    } else if (oper === "/") {
+        divide(num1, num2);
+    } else {
+        alert("Enter basic math operator(+,-,*,/)");
+    }
+}
+
+
+window.addEventListener('keydown', function(e) {
+    console.log(e);
+});
