5bfd8ed205d796f66f96e5128dcca37954d69898
Ante Dragoje <antekrtalic2@gmail.com>
2021-05-07 13:46:32 +0200

    Making changes for user being able to use calculator by clicking not just using buttons

--- script.js
+++ script.js
@@ -21,6 +21,25 @@ function divide(a, b) {
     
 }
 
+/* Dividing output for calculation */
+function evaluation(text) {
+    if (text.includes("+")) {
+        text = text.split("+");
+        text.splice(1, 0, "+");
+    } else if (text.includes("-")) {
+        text = text.split("-");
+        text.splice(1, 0, "-");
+    } else if (text.includes("*")) {
+        text = text.split("*");
+        text.splice(1, 0, "*");
+    } else if (text.includes("/")) {
+        text = text.split("/");
+        text.splice(1, 0, "/");
+    }
+   
+    return text;
+}
+
 /* Calculating numbers */
 function operate(oper, num1, num2) {
     let result;
@@ -119,116 +138,14 @@ window.addEventListener('keydown', function(e) {
         default:
             break;
     }
-    
-    paragraph.textContent = decimalPoint(paragraph.textContent);
+
     
 });
 
-// const divs = document.querySelectorAll('.key');
-// divs.forEach(div => {
-//     div.className += " active";
-// });
+/* Displaying output by pressing mouse */
 
-// console.log(divs);
+window.addEventListener('click', function(e) {
+    let div = document.getElementById(`${e.target.id}`);
+    
+})
 
-// divs.forEach(element => {
-//     element.addEventListener('keydown', function(e) {
-        
-//     switch(e.key)) {
-//         case "Delete":
-//             paragraph.textContent = "";
-//             break;
-//         case "Escape":
-//             paragraph.textContent = "";
-//             break;
-//         case "Backspace":
-//             console.log("da");
-//             paragraph.textContent = text.slice(0, text.length - 1);
-//             break;
-//         case "0":
-//             paragraph.textContent += "0";
-//             break;
-//         case "1": 
-//             paragraph.textContent += "1";
-//             break;
-//         case "2": 
-//             paragraph.textContent += "2";
-//             break;
-//         case "3": 
-//             paragraph.textContent += "3";
-//             break;
-//         case "4": 
-//             paragraph.textContent += "4";
-//             break;
-//         case "5": 
-//             paragraph.textContent += "5";
-//             break;
-//         case "6": 
-//             paragraph.textContent += "6";
-//             break;
-//         case "7": 
-//             paragraph.textContent += "7";
-//             break;
-//         case "8": 
-//             paragraph.textContent += "8";
-//             break;
-//         case "9": 
-//             paragraph.textContent += "9";
-//             break;
-//         case ",":
-//             paragraph.textContent += ".";
-//             break;
-//         case "+":
-//             paragraph.textContent += "+";
-//             break;
-//         case "-":
-//             paragraph.textContent += "-";
-//             break;
-//         case "*":
-//             paragraph.textContent += "*";
-//             break;
-//         case "/":
-//             paragraph.textContent += "/";
-//             break;
-//         case "Enter":
-//             values = evaluation(text);
-//             num1 = parseInt(values[0]);
-//             num2 = parseInt(values[2]);
-//             oper = values[1];
-//             if ((num1 && num2) || num1 === 0 || num2 === 0) paragraph.textContent = operate(oper, num1, num2);
-//             break;
-//         case "F9": 
-//             if (paragraph.textContent[0] === "-") paragraph.textContent = "+" + paragraph.textContent.slice(1);
-//             if (paragraph.textContent[0] !== "-") paragraph.textContent = "-" + paragraph.textContent.slice(0);
-//         default:
-//             break;
-//     }
-//     });
-// });
-
-function evaluation(text) {
-    if (text.includes("+")) {
-        text = text.split("+");
-        text.splice(1, 0, "+");
-    } else if (text.includes("-")) {
-        text = text.split("-");
-        text.splice(1, 0, "-");
-    } else if (text.includes("*")) {
-        text = text.split("*");
-        text.splice(1, 0, "*");
-    } else if (text.includes("/")) {
-        text = text.split("/");
-        text.splice(1, 0, "/");
-    }
-   
-    return text;
-}
-
-function decimalPoint(num_text) {
-    if (num_text.includes('.') && !(num_text.includes('+'))) {
-        if (num_text.indexOf('.') + 2 < num_text.length) {
-            num_text = num_text.slice(0, num_text.indexOf('.')+2);
-        }
-    }
-    return num_text;
-}
